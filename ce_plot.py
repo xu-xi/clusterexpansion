24aae3d0a27202429f1c25cefbcdd6832630dafe
Xu Xi <xuxi@pku.edu.cn>
2017-02-17 19:06:29 +0800

    test

--- ce_plot.py
+++ ce_plot.py
@@ -9,26 +9,25 @@ args=parser.parse_args()
 
 lat_atom_number=len(file('lat.in').readlines())-6
 datafile=file('%s.dat' % (args.property),'w')
-datafile.write('#index %s-ce %s-vasp\n' % (args.property,args.property))
+datafile.write('#index\t%s-ce\t%s-vasp\n' % (args.property,args.property))
 for item in os.listdir(os.environ['PWD']):
         fullpath=os.path.join(os.environ['PWD'],item)
         if os.path.isdir(fullpath):
                 os.chdir(fullpath)
                 if os.path.isfile(args.property) and not os.path.isfile('error'):
-                        #datafile.write(str(os.path.basename(fullpath))+' ')
+                        datafile.write(str(os.path.basename(fullpath))+'\t')
                         sc_atom_number=len(file('str.out').readlines())-6
                         vasp_data=float(subprocess.check_output('cat %s' %(args.property),shell=True))
                         #ce_energy=(sc_atom_number/lat_atom_number)*float(subprocess.check_output('corrdump -c -l=../lat.in -cf=../clusters.out -eci=../bandgap.eci',shell=True))
                         ce_data=float(subprocess.check_output('corrdump -c -mi -l=../lat.in -cf=../clusters.out -eci=../%s.ecimult' %(args.property),shell=True))
-                        datafile.write('%.5f %.5f\n' %(ce_data,vasp_data))
+                        datafile.write('%.5f\t%.5f\n' %(ce_data,vasp_data))
                         os.chdir('../')
 datafile.close()
 
-#os.chdir('../')
 data=numpy.loadtxt('%s.dat' % (args.property))
-#index=list(data[:,0])
-ce_data=list(data[:,0])
-vasp_data=list(data[:,1])
+index=list(data[:,0])
+ce_data=list(data[:,1])
+vasp_data=list(data[:,2])
 
 plt.xlabel('Fitted Bandgap/eV')
 plt.ylabel('Calculated Bandgap/eV')
@@ -52,7 +51,7 @@ plt.xlabel('Index of clusters')
 plt.ylabel('ECI/eV')
 plt.xlim(-0.5,len(eci)-0.5)
 #plt.xticks(numpy.arange(len(eci)))
-plt.xticks([2,10,13],['pair','trip','quad'])
+#plt.xticks([2,10,13],['pair','trip','quad'])
 
 plt.bar(numpy.arange(len(eci)),eci,color='.25',width=1)
 plt.axhline(y=0,linewidth=.5,c='k')

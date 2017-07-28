de8419a76684dce1ed2ab7659bb327d653cfa543
Xu Xi <xuxi@pku.edu.cn>
2017-07-28 11:04:40 +0800

    fix bugs

--- clusters_opt.py
+++ clusters_opt.py
@@ -4,13 +4,20 @@ from myfunc import read_clusters
 
 def clusters_optimizer(max_cluster_number,property_to_expand,average=False):
     'optimal cluster expansion construction'
-    pair_distance=0
+    pair_distance=0.5
     triplet_distance=0
     quad_distance=0
+    optimal_pair_distance=pair_distance
+    optimal_triplet_distance=triplet_distance
+    optimal_quad_distance=quad_distance
+
     cluster_number_list=[[0,0,0]]
     cv_list=[]
+    cv_file=file('cv.log','w')
+    cv_file.write('#number CV pair triplet quad\n')
 
     subprocess.check_call('corrdump -clus -2=0.5',shell=True)
+    cluster_number=int(subprocess.check_output('getclus | wc -l',shell=True))
     if average==False:
         cv_list.append(float(subprocess.check_output('clusterexpand -e -cv %s | tail -1' %(property_to_expand),shell=True)))
     else:
@@ -18,16 +25,22 @@ def clusters_optimizer(max_cluster_number,property_to_expand,average=False):
     #null_point_cluster_number=int(subprocess.check_output('getclus | wc -l',shell=True))
 
     while 1:
-        pair_distance+=0.5
-        for triplet_distance in numpy.arange(0,pair_distance,0.5):
-            for quad_distance in numpy.arange(0,triplet_distance,0.5):
+        pair_distance+=args.step
+        subprocess.check_call('corrdump -clus -2=%s' %(pair_distance),shell=True)
+        cluster_number=int(subprocess.check_output('getclus | wc -l',shell=True))
+        if cluster_number>max_cluster_number:
+            break
+        for triplet_distance in numpy.arange(0,pair_distance,args.step):
+            for quad_distance in numpy.arange(0,triplet_distance,args.step):
                 subprocess.check_call('corrdump -clus -2=%s -3=%s -4=%s' %(pair_distance,triplet_distance,quad_distance),shell=True)
+                #print pair_distance,triplet_distance,quad_distance
                 clusters=[]
                 for i in [2,3,4]:
                     a,b=read_clusters(i)
                     clusters.append(a)
                 cluster_number=int(subprocess.check_output('getclus | wc -l',shell=True))
-                if cluster_number>=max_cluster_number:
+                #print cluster_number
+                if cluster_number>max_cluster_number:
                     break
                 if clusters not in cluster_number_list:
                     cluster_number_list.append(clusters)
@@ -37,29 +50,27 @@ def clusters_optimizer(max_cluster_number,property_to_expand,average=False):
                         cv=float(subprocess.check_output('clusterexpand -e -cv %s | tail -1' %(property_to_expand),shell=True))
                     else:
                         cv=float(subprocess.check_output('clusterexpand -e -pa -cv %s | tail -1' %(property_to_expand),shell=True))
+                    cv_file.write('%s %s %s %s %s\n' %(cluster_number,cv,pair_distance,triplet_distance,quad_distance))
                     if cv<min(cv_list):
                         optimal_pair_distance=pair_distance
                         optimal_triplet_distance=triplet_distance
                         optimal_quad_distance=quad_distance
                     cv_list.append(cv)
-            else:
-                continue
-            break
-        else:
-            continue
-        #print cluster_number_list,cv_list
-        subprocess.check_call('corrdump -clus -2=%s -3=%s -4=%s' %(optimal_pair_distance,optimal_triplet_distance,optimal_quad_distance),shell=True)
-        if average==False:
-            optimal_cv=float(subprocess.check_output('clusterexpand -e -cv %s | tail -1' %(property_to_expand),shell=True))
-        else:
-            optimal_cv=float(subprocess.check_output('clusterexpand -e -pa -cv %s | tail -1' %(property_to_expand),shell=True))
-        return optimal_cv
+    
+    cv_file.close()
+    subprocess.check_call('corrdump -clus -2=%s -3=%s -4=%s' %(optimal_pair_distance,optimal_triplet_distance,optimal_quad_distance),shell=True)
+    if average==False:
+        optimal_cv=float(subprocess.check_output('clusterexpand -e -cv %s | tail -1' %(property_to_expand),shell=True))
+    else:
+        optimal_cv=float(subprocess.check_output('clusterexpand -e -pa -cv %s | tail -1' %(property_to_expand),shell=True))
+    return optimal_cv
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser(description='optimal cluster expansion construction',
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n',dest='max_clusters_number',type=int,help="the maximum number of clusters")
     parser.add_argument('-p',default='energy',dest='property',help="the property to expand")
+    parser.add_argument('-s',type=float,default=0.5,dest='step',help="the scan step of clusters")
     #parser.add_argument('-o',action='store_true',dest='opt_only',help="do not do cluster expansion, only optimize the choice of clusters")
     #parser.add_argument('--version',action='version',version='2017.2.23',help="output the version of the program")
 
